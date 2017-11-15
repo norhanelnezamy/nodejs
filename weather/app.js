f7789c87e45333d46588bbae3873b44bd4e6d5ea
norhanrlnezamy <norhanrlnezamy@gmail.com>
2017-11-15 18:52:47 +0200

    extract address, lat and lng from body

--- weather/app.js
+++ weather/app.js
@@ -4,5 +4,8 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=almansoura%20egypt',
     json: true
   }, (error, response, body) => {
-    console.log(body);
+    // console.log(JSON.stringify(body, undefined, 2));
+    console.log(` Address: ${ body.results[0].formatted_address }`);
+    console.log(` Lat: ${ body.results[0].geometry.location.lat }`);
+    console.log(` Lng: ${ body.results[0].geometry.location.lng }`);
 });
