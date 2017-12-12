adfb65bf31b329fdf5ea37d2b3c75eae4211a7d5
Norhan El-Nezamy <norhanelnezamy@gmail.com>
2017-12-12 23:36:05 +0200

    connect and disconnect to server socket

--- socket/server.js
+++ socket/server.js
@@ -1,10 +1,23 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, './public');
 const app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(8000, () => {
+io.on('connection', (socket) => {
+  console.log('New User Connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(8000, () => {
   console.log('listenning on port 8000');
 });
